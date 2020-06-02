5b971c43e968ed9a7338d012f9d1d8fa0c441330
frenchdonuts <jonathantan1991@gmail.com>
2020-06-02 15:51:21 -0700

    Stub view_notes/Interactors.searchNotes();

--- app/src/main/java/io/onedonut/backburner/view_notes/interactors/Interactors.kt
+++ app/src/main/java/io/onedonut/backburner/view_notes/interactors/Interactors.kt
@@ -2,7 +2,9 @@ package io.onedonut.backburner.view_notes.interactors
 
 import io.onedonut.backburner.view_notes.vm.VM
 import io.reactivex.Observable
+import io.reactivex.Single
 
 interface Interactors {
     fun meditations(): Observable<VM.Msg>
+    fun searchNotes(query: CharSequence): Single<VM.Msg>
 }
--- app/src/main/java/io/onedonut/backburner/view_notes/interactors/InteractorsImpl.kt
+++ app/src/main/java/io/onedonut/backburner/view_notes/interactors/InteractorsImpl.kt
@@ -3,6 +3,7 @@ package io.onedonut.backburner.view_notes.interactors
 import io.onedonut.backburner.dal.NoteRepository
 import io.onedonut.backburner.view_notes.vm.VM
 import io.reactivex.Observable
+import io.reactivex.Single
 import javax.inject.Inject
 
 class InteractorsImpl @Inject constructor(val noteRepo: NoteRepository) :
@@ -11,6 +12,11 @@ class InteractorsImpl @Inject constructor(val noteRepo: NoteRepository) :
     override fun meditations(): Observable<VM.Msg> {
         return noteRepo.all()
             .map { VM.Msg.NotesLoaded(it) }
+    }
+
+    override fun searchNotes(query: CharSequence): Single<VM.Msg> {
+        // TODO
+        return Single.just(VM.Msg.NotesSearchResult(listOf()))
     }
 
     companion object {
